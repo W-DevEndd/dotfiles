0793249c35e9274fe4fb6dd8502e3e1c483b99ab
W.DevEndd <111259650+W-DevEndd@users.noreply.github.com>
2026-03-01 06:24:03 +0700

    idk

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -2,4 +2,7 @@ return {
     require("config.plugins.catppuccin"),
     require("config.plugins.neo-tree"),
     require("config.plugins.nvim-treesitter"),
+    require("config.plugins.mason"),
+    require("config.plugins.nvim-lspconfig"),
+    require("config.plugins.completion-nvim"),
 }
--- nvim/lua/config/plugins/nvim-treesitter.lua
+++ nvim/lua/config/plugins/nvim-treesitter.lua
@@ -1,11 +1,26 @@
 
 return {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
     lazy = false,
     build = ':TSUpdate',
     config = function()
-        require('nvim-treesitter.configs').setup({
-            auto_install = true -- Ensure essential parsers are installed
-        })
-    end,
+	    local configs = require('nvim-treesitter.configs')
+        configs.setup({
+		    auto_install = true,
+		    ensure_installed = { "lua", "vim", "vimdoc" },
+		    highlight = {
+			    enable = true,
+            },
+
+            rainbow = {
+                enable = true,
+                -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+                extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+                max_file_lines = nil, -- Do not enable for files with more than n lines, int
+                -- colors = {}, -- table of hex strings
+                -- termcolors = {} -- table of colour name strings
+            }
+	    })
+    end
 }
