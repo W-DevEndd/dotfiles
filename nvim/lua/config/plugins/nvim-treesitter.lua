27063494b94512c1a7236e5b553c1c7605b0cd16
W-DevEndd <111259650+W-DevEndd@users.noreply.github.com>
2026-07-14 22:26:36 +0700

    feat(neovim): add code fold like vscode

--- nvim/lua/config/plugins/nvim-treesitter.lua
+++ nvim/lua/config/plugins/nvim-treesitter.lua
@@ -1,6 +1,11 @@
 
 return {
     'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+        "kevinhwang91/nvim-ufo",
+        "kevinhwang91/promise-async",
+    },
+
     branch = 'main',
     lazy = false,
     build = ':TSUpdate',
@@ -21,5 +26,45 @@ return {
                 -- termcolors = {} -- table of colour name strings
             }
 	    })
+
+        -- ufo
+        vim.o.foldcolumn = '1'
+        vim.o.foldlevel = 99
+        vim.o.foldlevelstart = 99
+        vim.o.foldenable = true
+
+        vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+        local ufo = require('ufo')
+
+        local handler = function(virtText, lnum, endLnum, width, truncate)
+            local newVirtText = {}
+            local suffix = (' ⋯ %d lines '):format(endLnum - lnum)
+            local sufWidth = vim.fn.strdisplaywidth(suffix)
+            local targetWidth = width - sufWidth
+            local curWidth = 0
+            for _, chunk in ipairs(virtText) do
+                local chunkText = chunk[1]
+                local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                if targetWidth > curWidth + chunkWidth then
+                    table.insert(newVirtText, chunk)
+                else
+                    chunkText = truncate(chunkText, targetWidth - curWidth)
+                    table.insert(newVirtText, {chunkText, chunk[2]})
+                    table.insert(newVirtText, {suffix, 'MoreMsg'})
+                    break
+                end
+                curWidth = curWidth + chunkWidth
+            end
+            return newVirtText
+        end
+
+        ufo.setup({
+            fold_virt_text_handler = handler,
+            provider_selector = function(bufnr, filetype, buftype)
+                return {'treesitter', 'indent'}
+            end
+        })
+
     end
 }
