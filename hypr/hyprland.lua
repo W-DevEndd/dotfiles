f3362add98c858c15a7c8af5cc209576e12a5fa7
W-DevEndd <111259650+W-DevEndd@users.noreply.github.com>
2026-08-08 14:02:54 +0700

    feat(hyprland): add screenshot bind

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -326,6 +326,9 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
+hl.bind("Print", hl.dsp.exec_cmd(
+    'DIR="$HOME/Pictures/Screenshots"; NAME="Screenshot_$(date +%Y-%m-%d_%H-%M-%S).png"; mkdir -p "$DIR"; grim "$DIR/$NAME" && satty --filename "$DIR/$NAME" --output-filename "$DIR/$NAME"'
+))
 
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
